8eb05e8d05e9054aae424b79cf9c73a189bb9d4e
Lovac42 <lovac42@protonmail.com>
2018-12-26 20:21:29 -0500

    fixed no card return value, separated code

---
+++ src/defuzz/const.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright: (C) 2018-2019 Lovac42
+# Support: https://github.com/lovac42/DeFuzz
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+# Version: 0.0.1
+
+
+from anki import version
+ANKI21 = version.startswith("2.1.")
