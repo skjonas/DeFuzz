087889229175b10319658073a622706322d1100a
Lovac42 <lovac42@protonmail.com>
2018-12-27 17:01:15 -0500

    read friendly

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # DeFuzz: Quick Effective Trimming
 
+<b>Note:</b> If you are updating, please check options values again as extra date ranges has been added.
+
 <img src="https://raw.githubusercontent.com/lovac42/DeFuzz/master/screenshots/defuzz.jpg" />  
 
 <img src="https://raw.githubusercontent.com/lovac42/DeFuzz/master/screenshots/ankiDef.png" />  
@@ -7,3 +9,6 @@
 <img src="https://raw.githubusercontent.com/lovac42/DeFuzz/master/screenshots/nofuzz.png" />  
 
 <img src="https://raw.githubusercontent.com/lovac42/DeFuzz/master/screenshots/sm19.png" />  
+
+## Configs:
+If you need peach fuzz ranges, you may edit const.py to add specific cutoff intervals.
--- src/defuzz/const.py
+++ src/defuzz/const.py
@@ -2,7 +2,7 @@
 # Copyright: (C) 2018-2019 Lovac42
 # Support: https://github.com/lovac42/DeFuzz
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.2
+# Version: 0.0.3
 
 
 from anki import version
@@ -18,19 +18,19 @@ FUZZ_LEVELS = (
 # == User Config =========================================
 
 # Add and remove lines as needed,
-# ( Level, fuzz_min, percent )
 
-    (7,1,25),  # <7, IVL 7, min 1, 25%
-    (14,2,15),
-    (21,2,15), # <21, IVL 21, min 2, 15%
-    (30,2,15),
-    (45,4,5),
-    (60,4,5),
-    (90,4,5),
-    (120,4,5),
-    (150,4,5),
-    (180,4,5),
-    (210,4,5),
+# ( Level, fuzz_min, percent )
+    (   7, 1, 25 ),  # <7, IVL 1-6, min 1, 25%
+    (  14, 2, 15 ),
+    (  21, 2, 15 ),  # <21, IVL 1-20, min 2, 15%
+    (  30, 2, 15 ),
+    (  45, 4,  5 ),
+    (  60, 4,  5 ),
+    (  90, 4,  5 ),  # <90, IVL 1-89, min 4, 5%
+    ( 120, 4,  5 ),
+    ( 150, 4,  5 ),
+    ( 180, 4,  5 ),
+    ( 210, 4,  5 ),
 
 # == End Config ==========================================
 ##########################################################
